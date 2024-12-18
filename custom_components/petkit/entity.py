85be951d74125ded50f3b774220794ddb1f1174f
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-12-18 14:54:56 +0100

    :building_construction: address SC maintenability rules

--- custom_components/petkit/entity.py
+++ custom_components/petkit/entity.py
@@ -6,6 +6,7 @@ from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any, Generic, TypeVar
 
+from pypetkitapi.containers import Pet
 from pypetkitapi.feeder_container import Feeder
 from pypetkitapi.litter_container import Litter
 from pypetkitapi.water_fountain_container import WaterFountain
@@ -17,7 +18,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN, LOGGER, PETKIT_DEVICES_MAPPING
 from .coordinator import PetkitDataUpdateCoordinator
 
-_DevicesT = TypeVar("_DevicesT", bound=Feeder | Litter | WaterFountain)
+_DevicesT = TypeVar("_DevicesT", bound=Feeder | Litter | WaterFountain | Pet)
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -31,28 +32,56 @@ class PetKitDescSensorBase(EntityDescription):
         None  # List of device types to force ever if not detected
     )
 
-    def is_supported(self, device: Feeder | Litter | WaterFountain) -> bool:
+    def is_supported(self, device: Feeder | Litter | WaterFountain | Pet) -> bool:
         """Check if the entity is supported by trying to execute the value lambda."""
 
+        if not isinstance(device, (Feeder, Litter, WaterFountain | Pet)):
+            LOGGER.error(
+                f"Device instance is not of expected type: {type(device)} can't check support"
+            )
+            return False
+
+        if self._is_force_added(device):
+            return True
+
+        if self._is_ignored(device):
+            return False
+
+        if self._is_not_in_supported_types(device):
+            return False
+
+        return self._check_value_support(device)
+
+    def _is_force_added(self, device: Feeder | Litter | WaterFountain) -> bool:
+        """Check if the device is in the force_add list."""
         if self.force_add:
-            for force_device in self.force_add:
-                if device.device_type.lower() == force_device:
-                    LOGGER.debug(f"{device.device_type} force add for '{self.key}'")
-                    return True
+            if device.device_type.lower() in self.force_add:
+                LOGGER.debug(f"{device.device_type} force add for '{self.key}'")
+                return True
+        return False
 
+    def _is_ignored(self, device: Feeder | Litter | WaterFountain) -> bool:
+        """Check if the device is in the ignore_types list."""
         if self.ignore_types:
-            for ignore_device in self.ignore_types:
-                if device.device_type.lower() == ignore_device:
-                    LOGGER.debug(f"{device.device_type} force ignore for '{self.key}'")
-                    return False
+            if device.device_type.lower() in self.ignore_types:
+                LOGGER.debug(f"{device.device_type} force ignore for '{self.key}'")
+                return True
+        return False
 
+    def _is_not_in_supported_types(
+        self, device: Feeder | Litter | WaterFountain
+    ) -> bool:
+        """Check if the device is not in the only_for_types list."""
         if self.only_for_types:
             if device.device_type.lower() not in self.only_for_types:
                 LOGGER.debug(
                     f"{device.device_type} is NOT COMPATIBLE with '{self.key}'"
                 )
-                return False
+                return True
+        return False
 
+    def _check_value_support(self, device: Feeder | Litter | WaterFountain) -> bool:
+        """Check if the device supports the value lambda."""
         if self.value is not None:
             try:
                 result = self.value(device)
@@ -61,7 +90,7 @@ class PetKitDescSensorBase(EntityDescription):
                         f"{device.device_type} DOES NOT support '{self.key}' (value is None)"
                     )
                     return False
-                LOGGER.debug(f"{device.device_type} support '{self.key}'")
+                LOGGER.debug(f"{device.device_type} supports '{self.key}'")
             except AttributeError:
                 LOGGER.debug(f"{device.device_type} DOES NOT support '{self.key}'")
                 return False
@@ -97,7 +126,7 @@ class PetkitEntity(CoordinatorEntity[PetkitDataUpdateCoordinator], Generic[_Devi
         return DeviceInfo(
             identifiers={(DOMAIN, self.device.sn)},
             manufacturer="Petkit",
-            model=f"{PETKIT_DEVICES_MAPPING.get(self.device.device_type.lower(), 'Unknown Device')} ({self.device.device_type})",
+            model=f"{PETKIT_DEVICES_MAPPING.get(self.device.device_type.lower(), 'Unknown Device')} ({self.device.device_type.upper()})",
             name=self.device.name,
             sw_version=str(self.device.firmware),
         )
