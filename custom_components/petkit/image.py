67d51dd2d96dcc8f1b694c87f57143d65d07c73b
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-12-21 11:39:09 +0100

    :recycle: use Path instead os.path, use async files access

--- custom_components/petkit/image.py
+++ custom_components/petkit/image.py
@@ -2,19 +2,19 @@
 
 from __future__ import annotations
 
-import asyncio
-import datetime
-import os
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any
+import datetime
+from pathlib import Path
+from typing import TYPE_CHECKING
 
-from homeassistant.components.image import ImageEntityDescription, ImageEntity
-from pypetkitapi.const import  D4H, D4SH
+import aiofiles
+from pypetkitapi.const import D4H, D4SH
 from pypetkitapi.feeder_container import Feeder
 from pypetkitapi.litter_container import Litter
-from pypetkitapi.water_fountain_container import WaterFountain
 from pypetkitapi.medias import MediaHandler
+from pypetkitapi.water_fountain_container import WaterFountain
 
+from homeassistant.components.image import ImageEntity, ImageEntityDescription
 
 from .const import LOGGER
 from .entity import PetKitDescSensorBase, PetkitEntity
@@ -54,12 +54,6 @@ IMAGE_MAPPING: dict[type[Feeder | Litter | WaterFountain], list[PetKitImageDesc]
             translation_key="last_feed_event",
             only_for_types=[D4SH, D4H],
         ),
-        # PetKitImageDesc(
-        #     key="Last move event",
-        #     event_key="move",
-        #     translation_key="last_move_event",
-        #     only_for_types=[D4SH, D4H],
-        # ),
     ],
     Litter: [],
     WaterFountain: [],
@@ -83,7 +77,7 @@ async def async_setup_entry(
         for device_type, entity_descriptions in IMAGE_MAPPING.items()
         if isinstance(device, device_type)
         for entity_description in entity_descriptions
-        if entity_description.is_supported(device)  # Check if the entity is supported
+        if entity_description.is_supported(device)
     ]
     async_add_entities(entities)
 
@@ -105,8 +99,8 @@ class PetkitImage(PetkitEntity, ImageEntity):
         self.coordinator = coordinator
         self.entity_description = entity_description
         self.device = device
-        self.media_handler = MediaHandler(device, os.path.join(os.path.dirname(__file__), 'images'))
-        self._last_image_timestamp = None  # Stocke le dernier timestamp
+        self.media_handler = MediaHandler(device, Path(__file__).parent / "images")
+        self._last_image_timestamp: datetime.datetime | None = None
 
     @property
     def unique_id(self) -> str:
@@ -120,22 +114,26 @@ class PetkitImage(PetkitEntity, ImageEntity):
         """Return timestamp of last image update."""
         return self._last_image_timestamp
 
-    def image(self) -> bytes | None:
-        """Return bytes of image."""
-        asyncio.run(self.media_handler.get_last_image())
+    async def async_image(self) -> bytes | None:
+        """Return bytes of image asynchronously."""
+        await self.media_handler.get_last_image()
         result = self.media_handler.media_files
         event_key = self.entity_description.event_key
-        filename, timestamp = self._get_filename_and_timestamp_for_event_key(result, event_key)
+        filename, timestamp = self._get_filename_and_timestamp_for_event_key(
+            result, event_key
+        )
 
         if filename:
             try:
-                self._last_image_timestamp = timestamp  # Met à jour le dernier timestamp
-                image_path = os.path.join(os.path.dirname(__file__), 'images', filename)
-                LOGGER.debug(f"Getting image for {self.device.device_type} Path is :{image_path}")
-                with open(image_path, 'rb') as image_file:
-                    return image_file.read()
+                self._last_image_timestamp = timestamp
+                image_path = Path(__file__).parent / "images" / filename
+                LOGGER.debug(
+                    f"Getting image for {self.device.device_type} Path is :{image_path}"
+                )
+                async with aiofiles.open(image_path, "rb") as image_file:
+                    return await image_file.read()
             except FileNotFoundError:
-                LOGGER.error('Image file not found')
+                LOGGER.error("Image file not found")
                 return None
         else:
             LOGGER.error(f"No filename found for event key '{event_key}'")
@@ -143,11 +141,11 @@ class PetkitImage(PetkitEntity, ImageEntity):
 
     @staticmethod
     def _get_filename_and_timestamp_for_event_key(media_files, event_key):
-        """
-        Parse media files and return the filename and timestamp for the given event key.
+        """Parse media files and return the filename and timestamp for the given event key.
 
         Returns:
             tuple: (filename, timestamp) or (None, None)
+
         """
         for media_file in media_files:
             if media_file.record_type == event_key:
