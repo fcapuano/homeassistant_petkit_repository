9af9d7a302f8583a01a0f815df8a82c10fe51ba8
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2025-01-06 21:19:55 +0100

    :sparkles: add diagnostics.py

--- custom_components/petkit/diagnostics.py
+++ custom_components/petkit/diagnostics.py
@@ -1,25 +1,16 @@
 """Petkit integration diagnostics."""
 
-from typing import TYPE_CHECKING
-
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntry
 
-if TYPE_CHECKING:
-    from homeassistant.core import HomeAssistant
-
-
-TO_REDACT = [
-    CONF_PASSWORD,
-    CONF_USERNAME
-]
+TO_REDACT = [CONF_PASSWORD, CONF_USERNAME]
 
 
 async def async_get_device_diagnostics(
-        hass: HomeAssistant, config_entry: ConfigEntry, device: DeviceEntry
+    hass: HomeAssistant, config_entry: ConfigEntry, device: DeviceEntry
 ) -> dict[str, any]:
     """Return diagnostics for a config entry."""
 
