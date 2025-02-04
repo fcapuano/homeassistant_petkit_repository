c199cd009728a9e09d0799a83e309ba464e8b5e2
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2025-02-04 18:07:04 +0100

    :memo: add docstring information

--- custom_components/petkit/utils.py
+++ custom_components/petkit/utils.py
@@ -6,7 +6,10 @@ from .const import EVENT_MAPPING, LOGGER
 
 
 def map_work_state(work_state: WorkState | None) -> str:
-    """Get the state of the litter box."""
+    """Get the state of the litter box.
+
+    Use the 'litter_state' translation table to map the state to a human readable string.
+    """
 
     LOGGER.debug(f"Litter map work_state: {work_state}")
 
@@ -102,7 +105,10 @@ def get_raw_feed_plan(feeder_records_data) -> str | None:
 
 
 def map_litter_event(litter_event: list[LitterRecord | None]) -> str | None:
-    """Return a description of the last event"""
+    """Return a description of the last event
+
+    Use the 'litter_last_event' translation table to map the state to a human readable string.
+    """
 
     if not isinstance(litter_event, list) or not litter_event:
         return None
